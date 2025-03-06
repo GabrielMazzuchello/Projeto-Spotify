9352b30754850730795828b7a91523cb6b94df5d
Gabriel Mazzuchello Dal Molin <94196035+GabrielMazzuchello@users.noreply.github.com>
2025-03-06 00:47:11 -0300

    apenas concertando o vs code

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
-
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/artists" element={<Artists />} />
